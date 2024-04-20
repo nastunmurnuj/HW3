1d9503a8e73745049ad741fc326cea4efdf5e67b
Anastasia <aan.kovalenko@gmail.com>
2024-04-20 15:39:51 +0200

    Adding homework

---
+++ hw3.js
@@ -0,0 +1,15 @@
+const myData = 	{
+    id: "0044",
+    name: "Alice 44",
+    address: {
+        "street": "1100 Main St",
+        "city": "City 100"
+    },
+    hobbies: [
+        "Reading 44",
+        "Hiking 44"
+    ]
+}
+
+
+console.log(myData.hobbies[0]);
